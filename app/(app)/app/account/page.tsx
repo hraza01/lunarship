0e4845d45f71e8df0cbe07d37deee168802a2cec
Hasan Raza <hasan.raza@blackfrosttechnologies.com>
2023-05-18 02:16:33 -0400

    fix: tab switching on account page

--- app/(app)/app/account/page.tsx
+++ app/(app)/app/account/page.tsx
@@ -5,9 +5,9 @@ import Settings from '@/app/(app)/app/account/Settings'
 import Funding from '@/app/(app)/app/account/Settings'
 
 const tabs = [
-  { name: 'Overview', href: '#', current: true },
-  { name: 'Funding', href: '#', current: false },
-  { name: 'Settings', href: '#', current: false },
+  { name: 'Overview', href: '#' },
+  { name: 'Funding', href: '#' },
+  { name: 'Settings', href: '#' },
 ]
 
 export default function Account() {
@@ -27,12 +27,12 @@ export default function Account() {
             <label htmlFor='tabs' className='sr-only'>
               Select a tab
             </label>
-            {/* Use an "onChange" listener to redirect the user to the selected tab URL. */}
             <select
               id='tabs'
               name='tabs'
               className='block w-full rounded-md border-none bg-white/5 py-2 pl-3 pr-10 text-base text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm'
-              defaultValue={tabs.find((tab) => tab.current).name}
+              defaultValue={page}
+              onChange={(e) => setPage(e.target.value.toLowerCase())}
             >
               {tabs.map((tab) => (
                 <option key={tab.name}>{tab.name}</option>
@@ -40,16 +40,22 @@ export default function Account() {
             </select>
           </div>
           <div className='hidden sm:block'>
-            <nav className='flex border-b border-white/10 py-4'>
+            <nav className='flex'>
               <ul
                 role='list'
-                className='flex min-w-full flex-none gap-x-6 px-2 text-sm font-semibold leading-6 text-gray-400'
+                className='flex min-w-full flex-none gap-x-6 border-b border-white/10 py-4 text-sm font-semibold leading-6 text-gray-400'
               >
                 {tabs.map((tab) => (
                   <li key={tab.name}>
                     <a
                       href={tab.href}
-                      className={tab.current ? 'text-indigo-400' : ''}
+                      className={`px-8 py-4 
+                        ${
+                          tab.name.toLowerCase() === page
+                            ? 'rounded-t border-b-2 border-indigo-400 bg-lunarship-gray-100 text-indigo-400'
+                            : ''
+                        }
+                      `}
                       name={tab.name}
                       onClick={tabClickHandler}
                     >
